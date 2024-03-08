8a6e5f466822de4b2a639012e50d89f58779817c
Eric Nguyen <nericguyen@gmail.com>
2024-03-07 16:22:58 -0800

    bruh

--- TestListExamples.java
+++ TestListExamples.java
@@ -18,4 +18,13 @@ public class TestListExamples {
     List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
     assertEquals(expected, merged);
   }
+
+  @Test
+  public void testListExamplesFilter() {
+    List<String> twoMoons = Arrays.asList("a", "b", "c", "moon", "moon");
+    List<String> expected = Arrays.asList("moon", "moon");
+    IsMoon sc = new IsMoon();
+    List<String> filtered = ListExamples.filter(twoMoons,sc);
+    assertEquals(expected, filtered);
+  }
 }
--- grade.sh
+++ grade.sh
@@ -8,9 +8,43 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+filename=$(find . -name ListExamples.java)
 
-# Draw a picture/take notes on the directory structure that's set up after
-# getting to this point
+if [[ -f ./student-submission/$filename ]]
+then
+    echo "File found! Running Tests."
+else
+    echo "File not found, please check your file name/submission"
+    exit
+fi
 
-# Then, add here code to compile and run, and do any post-processing of the
-# tests
+cp student-submission/ListExamples.java grading-area 
+cp TestListExamples.java grading-area
+cp -r lib grading-area
+
+pwd
+  cd grading-area
+pwd
+
+javac -cp $CPATH *.java
+if [[ $? -ne 0 ]]
+then
+  echo "The program failed to compile! Loser!"
+  exit 1
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > junit-output.txt
+
+allSuccess=$(grep -i "OK (" junit-output.txt)
+if [[ $? -eq 0 ]]
+then 
+  allSuccessReturn=$(echo $allSuccess | awk -F'[ (]' '{print $3}')
+  echo "All tests passed, your score is $allSuccessReturn / $allSuccessReturn"
+  exit 0
+fi
+
+lastline=$(grep -i "Tests run:" junit-output.txt)
+tests=$(echo $lastline | awk -F'[, ]' '{print $3}')
+failures=$(echo $lastline | awk -F'[, ]' '{print $6}')
+successes=$((tests - failures))
+echo "Your score is $successes / $tests"
